fc96227148e06585cabe7fc1d58630504a8de482
chenlinggg <1489789468@qq.com>
2024-09-21 23:28:14 +0800

    第一次提交

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+   <login-card :code="fathernum"></login-card>
+  </div>
+</template>
+
+<script>
+import LoginCard from './components/LoginCard.vue';
+
+export default {
+  name: 'App',
+  data(){
+   
+     return{
+       fathernum:'fdfdf'
+     }
+   
+  },
+  components: {
+    LoginCard
+  }
+}
+</script>
+
+<style>
+
+
+
+
+</style>
